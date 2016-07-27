ad3461e567cb9990f83b072bddf4dc4e1814b512
yuan_gao <yuan_gao@cdmail.sonix.com.cn>
2016-07-27 18:11:08 +0800

    tittle

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: '恒骏丰补胎匠' });
 });
 
 router.get('/:spec', function (req, res) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: '恒骏丰补胎匠' });
 });
 
 //for angular partial page service
 router.get('/partial/:spec', function (req, res) {
-	res.render(req.params.spec, { title: 'Express' });
+	res.render(req.params.spec, { title: '恒骏丰补胎匠' });
 });
 
 module.exports = router;
